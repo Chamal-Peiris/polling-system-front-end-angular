35f0ff92913e89cfbdb60cd9548c28505f7455a1
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-09 19:46:20 +0530

    Add routing and finalize the login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {UserService} from "../services/user-service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -12,16 +13,20 @@ export class LoginComponent implements OnInit {
   invalid=false;
   @ViewChild("txtName")
   txtName!:ElementRef<HTMLInputElement>;
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,private routerservice:Router) {
   }
 
   ngOnInit(): void {
+   if( this.userService.getPrincipal()){
+     this.routerservice.navigateByUrl('/dashboard');
+   }
   }
 
   enter(): void {
     if (this.userService.login(this.name)) {
       alert('Goda');
       this.invalid=false;
+      this.routerservice.navigateByUrl('/dashboard')
     } else {
       alert('Kachal');
       this.invalid=true;
